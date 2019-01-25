36a03c77a304f1349ff800669b05b0f4ab46ba3f
Josh Chandler <j5d1c7@gmail.com>
2019-01-25 18:24:45 -0500

    updated to newer version of lib

--- leds.py
+++ leds.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 import json, requests, time
-from neopixel import *
+from rpi_ws281x import *
 
 # LED strip configuration:
 LED_COUNT      = 300      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 96      # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -25,8 +25,8 @@ while True:
     colors = json.loads(r.text)
     print("GET: " + str(colors))
 
-    for i in range(0, 300):
-        strip.setPixelColor(i, Color(int(colors['green']), int(colors['red']), int(colors['blue'])))
+    for i in range(0, LED_COUNT):
+        strip.setPixelColor(i, Color(int(colors['red']), int(colors['green']), int(colors['blue'])))
 
     strip.show()
     time.sleep(3)
